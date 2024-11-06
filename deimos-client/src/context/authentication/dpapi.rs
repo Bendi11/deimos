c5d654bfca40951c91f4a19d5ede8869473f0848
Bendi11 <bkliebmann@gmail.com>
2024-11-05 16:57:57 -0800

    Switched to windows crate for DPAPI support

--- deimos-client/src/context/authentication/dpapi.rs
+++ deimos-client/src/context/authentication/dpapi.rs
@@ -1,29 +1,33 @@
-use winapi::{
-    um::{
-        wincrypt::CRYPTOAPI_BLOB,
-        dpapi::CryptProtectData,
-    },
-    shared::miniwindef::DWORD,
-};
+use fltk::{prelude::WindowExt, window::Window};
 
-pub fn dpapi_protect(buf: &[u8]) -> Result<Vec<u8>, DpapiError> {
-    let mut blob = CRYPTOAPI_BLOB {
-        cbData: buf.len() as DWORD,
-        pbData: buf.as_ptr(),
+use windows_core::w;
+use windows::Win32::{Foundation::HWND, Security::Cryptography::{self, CryptProtectData, CRYPTPROTECT_PROMPTSTRUCT, CRYPTPROTECT_PROMPT_ON_UNPROTECT, CRYPT_INTEGER_BLOB}};
+
+
+pub fn dpapi_protect(handle: &Window, buf: Vec<u8>) -> Result<Vec<u8>, DpapiError> {
+    let blob = CRYPT_INTEGER_BLOB {
+        cbData: buf.len() as u32,
+        pbData: buf.as_mut_ptr()
     };
 
-    let mut blob_out = CRYPTOAPI_BLOB::default();
+    let mut encrypt = CRYPT_INTEGER_BLOB {
+        cbData: 0,
+        pbData: std::ptr::null_mut(),
+    };
 
-    let rc = unsafe {
+    let result = unsafe {
         CryptProtectData(
-            &mut block as *mut CRYPTOAPI_BLOB,
-            std::ptr::null(),
-            std::ptr::null(),
-            std::ptr::null(),
-
-        )
-    }
+            std::ptr::from_ref(&blob),
+            w!("Deimos API Token"),
+            None,
+            None,
+            None,
+            0,
+            std::ptr::from_mut(&mut encrypt),
+        );
+    };
 }
+
 
 #[derive(Debug, thiserror::Error)]
 pub enum DpapiError {
